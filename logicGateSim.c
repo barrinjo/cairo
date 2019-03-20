bc78a87f7cc77a7c9ea5b0c6eb69b1e833f530bb
Joshua Barringer <jbarringer1999@gmail.com>
2019-03-20 11:58:22 -0700

    moving boxes with middle mouse.  still cant drag things

--- logicGateSim.c
+++ logicGateSim.c
@@ -7,10 +7,11 @@ static void do_drawing(cairo_t *);
 // array of clicked points
 struct {
 	int count;
-	double coordx[100];
-	double coordy[100];
+	double coordx[256];
+	double coordy[256];
 } glob;
-
+gboolean boxInMotion = FALSE;
+int movingBox;
 
 // connects the gtk event widget to the draw function in cairo
 static gboolean on_draw_event(GtkWidget *widget,
@@ -21,6 +22,19 @@ static gboolean on_draw_event(GtkWidget *widget,
 	return FALSE;
 }
 
+static int check_boxes (float x, float y) {
+	// iterates through the boxes and checks the mouse coordinates to see
+	// if the mouse is in the boxes
+	int i;
+	for(i = 0; i < glob.count; i++) {
+		if(x < glob.coordx[i]+20 && x > glob.coordx[i] - 20) {
+			if(y < glob.coordy[i]+20 && y > glob.coordy[i] - 20) {
+				return i+1;
+			}
+		}
+	}
+	return 0;
+}
 
 static float rgb(float x) {
 	x = x/255;
@@ -31,6 +45,7 @@ static float rgb(float x) {
 static void do_drawing(cairo_t *cr) {
 	// sets source to pink
 	cairo_set_source_rgb(cr, rgb(47), rgb(130), rgb(28));
+	cairo_set_line_join(cr, CAIRO_LINE_JOIN_ROUND);
 	cairo_set_line_width(cr, 5);
 
 	//for each clicked coordinate in glob.coord, draws a rectangle at that point
@@ -42,16 +57,35 @@ static void do_drawing(cairo_t *cr) {
 	cairo_stroke(cr);
 }
 
+static void moveBox(int i, int x, int y) {
+	glob.coordx[i] = x;
+	glob.coordy[i] = y;
+}
+
 static gboolean clicked(GtkWidget *widget,
 			GdkEventButton *event,
+			GdkEventMotion *motion,
 			gpointer user_data) {
-	//left click
-	if(event->button == 1) {
+
+	int boxLoc = check_boxes(event->x, event->y);
+
+	if(event->button == 2 && boxInMotion == TRUE) {
+		moveBox(movingBox, event->x, event->y);
+		boxInMotion = FALSE;
+	}
+
+	// left click outside of box
+	if(event->button == 2 && boxLoc && boxInMotion == FALSE) {
+		movingBox = boxLoc - 1;		
+		boxInMotion = TRUE;
+	}
+
+	//left click outside box
+	if(event->button == 1 && !check_boxes(event->x, event->y)) {
 		//adds coord to glob
 		glob.coordx[glob.count] = event->x;
 		glob.coordy[glob.count++] = event->y;
-		// redraws systemn
-		gtk_widget_queue_draw(widget);
+		// printf("ugh\n");
 	}
 
 	if(event->button == 3) {
@@ -60,6 +94,10 @@ static gboolean clicked(GtkWidget *widget,
 		gtk_widget_queue_draw(widget);
 	}
 
+	// if(boxInMotion)
+	// 	moveBox(boxLoc, event->x, event->y);
+
+	gtk_widget_queue_draw(widget);
 	return TRUE;
 }
 
